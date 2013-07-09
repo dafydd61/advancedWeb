dcd30fccdfb7ce94cc7e4a45b0ca103615e116ec
Dafydd Hughes <d@fancydavid.com>
2013-07-09 18:49:51 -0400

    Class 1 - Ohai

---
+++ 01-ohai/result.php
@@ -0,0 +1,16 @@
+<?php
+$answer = 6 * 7;
+$questionAndAnswer = "What is 6 times 9? " . $answer;
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Answer</title>
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+	<h1><?php echo($questionAndAnswer); ?></h1>
+</body>
+</html>
