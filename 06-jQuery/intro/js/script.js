793efd76d551d3941f6797da43e041b9b16ddf54
Dafydd Hughes <d@fancydavid.com>
2013-07-25 12:34:46 -0400

    Tidy up

--- 06-jQuery/intro/js/script.js
+++ 06-jQuery/intro/js/script.js
@@ -1,42 +1,6 @@
+document.write("<p>Hello</p>");
+console.log("4 x 3 = " + 4*3);
+
 $(document).ready(function() {
-
-  $('.reveal-trigger').click(function(event) {
-    event.preventDefault();
-    var target = $(this).attr('href');
-    $(target).slideToggle();
-  });
-
-  $('.slide-out-trigger').click(function(event) {
-    event.preventDefault();
-    var target = $(this).attr('href');
-    $(target).addClass('slide-out-content-is-visible');
-  });
-
-  $('.slide-out-close').click(function(event) {
-    event.preventDefault();
-    var target = $(this).attr('href');
-    $(target).removeClass('slide-out-content-is-visible');
-  });
-
-  $('#showme').click(function(event) {
-    event.preventDefault();
-    $(this).html("Whoa! That was cool!");
-  });
-
-  $('#add-more').click(function(event) {
-    event.preventDefault();
-    $(this).parent().append(' Hey look - more content!');
-  });
-
-  $('#holy-grail-form').submit(function() {
-    var form = $(this);
-    var action = $(this).attr('action');
-    $.get(action, $(this).serialize(), function(data) {
-      form.fadeOut(function() {
-        form.after(data);
-      });
-    });
-    return false;
-  });
 
 });
