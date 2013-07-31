1b8a0e06cf0a78917b745ad3b479dc2d01153475
Dafydd Hughes <d@fancydavid.com>
2013-07-31 14:39:59 -0400

    use db signon include

--- 08a-rocks/_signon.php
+++ 08a-rocks/_signon.php
@@ -1,4 +1,4 @@
 <?php
-require('_signon.php');
-require('_signon.php');
+mysql_connect("localhost", "root", "root") or die(mysql_error());
+mysql_select_db("rock_collector") or die(mysql_error());
 ?>
